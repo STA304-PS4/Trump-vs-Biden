6a3dddff1b53818ec9e5cfbed7fde757f3c7f828
Arjun <arjun.dhatt@mail.utoronto.ca>
2020-10-29 18:43:57 -0400

    6.43

---
+++ Data Cleaning.R
@@ -0,0 +1,50 @@
+#### Preamble ####
+# Purpose: Prepare and clean the survey data downloaded from [...UPDATE ME!!!!!]
+# Author: Arjun Dhatt, Ben Draskovic, Gantavya Gupta, Yiqu Ding 
+# Data: 2 October 2020
+# Contact: arjun.dhatt@mail.utoronto.ca 
+# License: MIT
+# Pre-requisites: 
+# - Need to have downloaded the data from X and save the folder that you're 
+# interested in to inputs/data 
+# - Don't forget to gitignore it!
+
+
+#### Workspace setup ####
+library(haven)
+library(tidyverse)
+# Read in the raw data (You might need to change this if you use a different dataset)
+raw_data <- read_dta("/Users/arjundhatt/Trump-vs-Biden/ns20200625.dta")
+# Add the labels
+raw_data <- labelled::to_factor(raw_data)
+# Just keep some variables
+reduced_data <- 
+  raw_data %>% 
+  select(interest,
+         registration,
+         vote_2016,
+         vote_intention,
+         vote_2020,
+         ideo5,
+         employment,
+         foreign_born,
+         gender,
+         census_region,
+         hispanic,
+         race_ethnicity,
+         household_income,
+         education,
+         state,
+         congress_district,
+         age)
+
+ps3_data <- 
+  raw_data %>% 
+  select(age,
+         education, hispanic,household_income, race_ethnicity,extra_covid_worn_mask,state, trump_biden
+         )
+
+#### What else???? ####
+# Maybe make some age-groups?
+# Maybe check the values?
+# Is vote a binary? If not, what are you going to do?
