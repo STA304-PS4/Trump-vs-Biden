65e3627734300d5474c8b378eac461f8f45c61e8
Arjun <arjun.dhatt@mail.utoronto.ca>
2020-11-02 19:54:36 -0500

    7.54

--- scripts/01-data-cleaning-survey.R
+++ scripts/01-data-cleaning-survey.R
@@ -56,7 +56,7 @@ tbdata <-
 # NA value. 
 tbdata <- na.omit(tbdata)
 
-#Group all of the Asian and PAcific Islander Ethnicity options in to 3 categories
+#Group all of the Asian and Pacific Islander Ethnicity options in to 3 categories to make it easier to analyze
 recode_key_race <- c(  
   "Asian (Asian Indian)" = "asian or pacific islander",
   "Asian (Chinese)" = "asian or pacific islander",
@@ -78,7 +78,7 @@ recode_key_race <- c(
 # After the key is made use the recode function to create a new string based on the collumn being changed 
 race_ethnicity0 <- recode(tbdata$race_ethnicity, !!!recode_key_race)
 
-#Group the incomes in to groups that are easier to display
+#Group income so that it is easier to analyze. 
 recode_key_income <- c(  
   "Less than $14,999" = "$25k or less",
   "$15,000 to $19,999" = "$25k or less",
@@ -174,15 +174,7 @@ tbdata <- tbdata %>%
 
 write_csv(tbdata, "outputs/clean-survey.csv")
 
-
-#### What else???? ####
-# Maybe make some as.numeric(age)-groups?
-# Maybe check the values?
-# Is vote a binary? If not, what are you going to do?
-
-
-
-# The vote is not a binary variable, it contains 758 "Don't Know", 2626 "Donald Trump", 3075 "Joe Biden" and 20 NAs. 
+# Vote is not a binary variable in our data. It contains 758 "Don't Know", 2626 "Donald Trump", 3075 "Joe Biden" and 20 NAs. 
 # We resolved this by deleting the "Don't know" and NA responses, since it was a relatively small (12%) part of our
-# data set.
+# data set. This makes the dataset much simpler and easier to analyze. 
 
